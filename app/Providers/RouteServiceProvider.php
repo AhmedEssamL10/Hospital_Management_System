39578e78e9da8b42bc40b3ba49d8ef205740f873
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-10-14 18:42:13 +0300

    login and logout

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -35,7 +35,7 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-                Route::middleware('web')
+            Route::middleware('web')
                 ->group(base_path('routes/dashboard.php'));
         });
     }
