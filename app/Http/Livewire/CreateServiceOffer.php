7babe8839bcc051081005e0863c6fe1c686e6020
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-11-02 21:26:16 +0200

    update

--- app/Http/Livewire/CreateServiceOffer.php
+++ app/Http/Livewire/CreateServiceOffer.php
@@ -10,31 +10,18 @@ use Illuminate\Support\Facades\App;
 class CreateServiceOffer extends Component
 {
     public $en_name;
-    public $count = 0;
-
     public $ar_name;
     public $en_desc;
     public $ar_desc;
-    public function increment()
+    public $discount_value = 0;
+    public $tax_value = 0;
+    public function create()
     {
-        $this->count++;
+        
     }
-
-    public function save()
-    {
-        Section::create([
-            'en_name' => $this->en_name,
-            'ar_name' => $this->ar_name,
-            'en_desc' => $this->en_desc,
-            'ar_desc' => $this->ar_desc
-        ]);
-        // dd($this->en_name);
-    }
-
     public function render()
-
     {
-        $sections = Section::select('id', App::getLocale() . '_name As name')->get();
-        return view('livewire.service-offer.create-service-offer', compact('sections'));
+        $services = Service::select('id', App::getLocale() . '_name As name', 'price')->get();
+        return view('livewire.service-offer.create-service-offer', compact('services'));
     }
 }
--- resources/views/livewire/service-offer/create-service-offer.blade.php
+++ resources/views/livewire/service-offer/create-service-offer.blade.php
@@ -1,15 +1,28 @@
 <div>
-    <button wire:click="increment">+</button>
+    {{-- <button wire:click="increment">+</button>
     <h1>{{ $count }}</h1>
     @foreach ($sections as $section)
         {{ $section->name }}
     @endforeach
-    {{-- <form wire:submit.prevent="save"> --}}
-    {{-- @csrf --}}
+    
     <input type="text" wire:model="en_name">
     <input type="text" wire:model='ar_name'>
     <input type="text" wire:model='en_desc'>
     <input type="text" wire:model='ar_desc'>
-    <button wire:click.prevent='save'>save</button>
-    {{-- </form> --}}
+    <button wire:click.prevent='save'>save</button> --}}
+
+    <form wire:submit.prevent="save">
+        @csrf
+        <label for="services" style="padding-left: 2%">services</label>
+        @foreach ($services as $service)
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="flexCheckChecked" name="services[]"
+                    value="{{ $service->id }}">
+                <label class="form-check-label" for="flexCheckChecked">
+                    {{ $service->name }}
+                </label>
+            </div>
+        @endforeach
+    </form>
+
 </div>
