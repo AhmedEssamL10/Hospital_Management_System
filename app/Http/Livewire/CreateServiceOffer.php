4a9d1f93dd580af3de69eab919c0ccd565ba3cca
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-11-04 09:25:56 +0200

    update

--- app/Http/Livewire/CreateServiceOffer.php
+++ app/Http/Livewire/CreateServiceOffer.php
@@ -21,6 +21,7 @@ class CreateServiceOffer extends Component
     public $tax_value = 0;
     public $total = 0;
     public $service = [];
+    public $offerId;
     public function create()
     {
 
@@ -70,5 +71,64 @@ class CreateServiceOffer extends Component
     public function deleteOffer($id)
     {
         Offer::where('id', $id)->delete();
+    }
+    public function setValues($id)
+    {
+        $offer = Offer::find($id);
+        if ($offer) {
+            $this->en_name = $offer->en_name;
+            $this->ar_name = $offer->ar_name;
+            $this->en_desc = $offer->en_desc;
+            $this->ar_desc = $offer->ar_desc;
+            $this->tax_value = $offer->tax_rate;
+            $this->discount_value = $offer->discount_value;
+            $this->offerId = $offer->id;
+            $this->service = [];
+            $this->total_before_descount = 0;
+            foreach ($offer->services->pluck('id') as $serviceid) {
+                $this->service[] = $serviceid;
+                $price = Service::find($serviceid);
+                $this->total_before_descount += $price->price;
+            }
+        }
+    }
+    public function updateOffer($id)
+    {
+        try {
+            $offer = Offer::where('id', $id)->update([
+                'en_name' => $this->en_name,
+                'ar_name' => $this->ar_name,
+                'en_desc' => $this->en_desc,
+                'ar_desc' => $this->ar_desc,
+                'tax_rate' => $this->tax_value,
+                'discount_value' => $this->discount_value,
+                'total_before_discount' => $this->total_before_descount,
+                'total_after_discount' => $this->total_before_descount - $this->discount_value,
+                'total' => $this->total_before_descount - $this->discount_value + $this->tax_value,
+            ]);;
+            DB::table('offer_service')->where('offer_id', $id)->delete();
+            foreach ($this->service as $key) {
+                DB::table('offer_service')->insert([
+                    'offer_id' => $id,
+                    'service_id' => $key
+                ]);
+            }
+
+
+            DB::commit();
+            // reset inputs
+            $this->offerId = null;
+            $this->en_name = '';
+            $this->ar_name = '';
+            $this->en_desc = '';
+            $this->ar_desc = '';
+            $this->service = [];
+            $this->discount_value = 0;
+            $this->tax_value = 0;
+            $this->total_before_descount = 0;
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollback();
+        }
     }
 }
--- resources/views/livewire/service-offer/create-service-offer.blade.php
+++ resources/views/livewire/service-offer/create-service-offer.blade.php
@@ -1,89 +1,99 @@
 <div>
-    <form wire:submit.prevent="create">
-        @csrf
-        <div class="row row-sm">
-            <div class="col-xl-12">
-                <div class="card">
-                    <div class="card-header pb-0">
-                        <div class="d-flex justify-content-between">
-                            <h4 class="card-title mg-b-0">Services</h4>
-                            <i class="mdi mdi-dots-horizontal text-gray"></i>
-                        </div>
+    @if ($offerId)
+        <form wire:submit.prevent="updateOffer({{ $offerId }})">
+            @csrf
+        @else
+            <form wire:submit.prevent="create">
+                @csrf
+    @endif
+    <div class="row row-sm">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="card-header pb-0">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="card-title mg-b-0">Services</h4>
+                        <i class="mdi mdi-dots-horizontal text-gray"></i>
                     </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table text-md-nowrap" id="example3">
-                                <thead>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table text-md-nowrap" id="example3">
+                            <thead>
+                                <tr>
+                                    <th class="wd-15p border-bottom-0">#</th>
+                                    <th class="wd-15p border-bottom-0">Select</th>
+                                    <th class="wd-15p border-bottom-0">Service Name</th>
+                                    <th class="wd-20p border-bottom-0">Service Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($services as $service)
                                     <tr>
-                                        <th class="wd-15p border-bottom-0">#</th>
-                                        <th class="wd-15p border-bottom-0">Select</th>
-                                        <th class="wd-15p border-bottom-0">Service Name</th>
-                                        <th class="wd-20p border-bottom-0">Service Price</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($services as $service)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox"
-                                                        id="flexCheckChecked" wire:model="service"
-                                                        value="{{ $service->id }}" wire:click="selectServices">
-                                                    <label class="form-check-label" for="flexCheckChecked">
-                                                    </label>
-                                                </div>
-                                            </td>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" id="flexCheckChecked"
+                                                    wire:model="service" value="{{ $service->id }}"
+                                                    wire:click="selectServices">
+                                                <label class="form-check-label" for="flexCheckChecked">
+                                                </label>
+                                            </div>
+                                        </td>
 
-                                            <td> {{ $service->name }}</td>
-                                            <td> {{ $service->price }} $</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                        <td> {{ $service->name }}</td>
+                                        <td> {{ $service->price }} $</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6 mb-3">
-                <label for="basic-url" class="form-label">En Name</label>
-                <input type="text" class="form-control" wire:model="en_name">
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="basic-url" class="form-label">Ar Name</label>
-                <input type="text" class="form-control" wire:model="ar_name">
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="basic-url" class="form-label">En Describtion</label>
-                <input type="text" class="form-control" wire:model='en_desc'>
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="basic-url" class="form-label">Ar Describtion</label>
-                <input type="text" class="form-control" wire:model='ar_desc'>
-            </div>
-            <div class="col-md-6 mb-3">
-                <label for="basic-url" class="form-label">Tax</label>
-                <input type="number" class="form-control" wire:model='tax_value'>
-            </div>
-            <div class="col-md-6 mb-3 ">
-                <label for="basic-url" class="form-label">Discount Value</label>
-                <input type="number" class="form-control" wire:model='discount_value'>
-            </div>
-            <div class="card col-md-6 mb-3 ml-3" style="width: 18rem;">
-                <div class="card-header">
-                    Total before discount: {{ $total_before_descount }}
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Total after discount: {{ $total_before_descount - $discount_value }}
-                    </li>
-                    <li class="list-group-item">Tax: {{ $tax_value }}</li>
-                    <li class="list-group-item">Total: {{ $total_before_descount - $discount_value + $tax_value }}</li>
-                </ul>
-            </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="basic-url" class="form-label">En Name</label>
+            <input type="text" class="form-control" wire:model="en_name">
         </div>
+        <div class="col-md-6 mb-3">
+            <label for="basic-url" class="form-label">Ar Name</label>
+            <input type="text" class="form-control" wire:model="ar_name">
+        </div>
+        <div class="col-md-6 mb-3">
+            <label for="basic-url" class="form-label">En Describtion</label>
+            <input type="text" class="form-control" wire:model='en_desc'>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label for="basic-url" class="form-label">Ar Describtion</label>
+            <input type="text" class="form-control" wire:model='ar_desc'>
+        </div>
+        <div class="col-md-6 mb-3">
+            <label for="basic-url" class="form-label">Tax</label>
+            <input type="number" class="form-control" wire:model='tax_value'>
+        </div>
+        <div class="col-md-6 mb-3 ">
+            <label for="basic-url" class="form-label">Discount Value</label>
+            <input type="number" class="form-control" wire:model='discount_value'>
+        </div>
+        <div class="card col-md-6 mb-3 ml-3" style="width: 18rem;">
+            <div class="card-header">
+                Total before discount: {{ $total_before_descount }}
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Total after discount: {{ $total_before_descount - $discount_value }}
+                </li>
+                <li class="list-group-item">Tax: {{ $tax_value }}</li>
+                <li class="list-group-item">Total: {{ $total_before_descount - $discount_value + $tax_value }}</li>
+            </ul>
+        </div>
+    </div>
+    @if ($offerId)
+        <button type="submit" class="btn btn-primary mb-3">Update</button>
+    @else
         <button type="submit" class="btn btn-primary mb-3">Create</button>
+    @endif
     </form>
     <div class="row row-sm">
         <div class="col-xl-12">
@@ -123,7 +133,9 @@
                                         <td> <button class="modal-effect btn btn-sm btn-danger"
                                                 wire:click="deleteOffer({{ $offer->id }})"
                                                 data-effect="effect-scale"><i class="las la-trash"></i></button>
-
+                                            <button class="modal-effect btn btn-sm btn-info"
+                                                wire:click="setValues({{ $offer->id }})"
+                                                data-effect="effect-scale"><i class="las la-pen"></i></button>
                                         </td>
                                     </tr>
                                 @endforeach
