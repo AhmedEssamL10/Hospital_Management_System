e3f8e6c76239e7d561b4e754edb4b98df4d10b55
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2023-11-05 10:50:11 +0200

    insurance table

---
+++ database/migrations/2023_11_05_084030_create_insurances_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insurances', function (Blueprint $table) {
+            $table->id();
+            $table->string('insurance_code')->unique();
+            $table->string('en_name', 50);
+            $table->string('ar_name', 50);
+            $table->string('discount_percentage');
+            $table->string('company_rate');
+            $table->string('en_note');
+            $table->string('ar_note');
+            $table->enum('status', ['0', '1'])->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insurances');
+    }
+};
